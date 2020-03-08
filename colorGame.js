676f752630499421462c9d4c69799a75f304eb8e
Umit Tokmak <umittokmak@gmail.com>
2020-03-09 00:29:10 +0100

    Random color picker

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor;
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +17,26 @@ for(var i=0; i<squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         // compare color to pickedColor
         if(clickedColor === pickedColor){
-            alert("Correct");
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         } else {
             this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again"
         }
     })
 }
+
+function changeColors(color){
+    // loop through all sqaures
+    for(var i=0; i<squares.length; i++){
+    // change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    // pick a random number
+    var random = Math.floor(Math.random() * colors.length );
+    return colors[random];
+}
+
