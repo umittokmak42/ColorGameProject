27549e8ee3802b7bd4b24a25f7a2cffcfdf443d3
Umit Tokmak <umittokmak@gmail.com>
2020-03-09 00:52:32 +0100

    added random colors

--- colorGame.js
+++ colorGame.js
@@ -1,9 +1,10 @@
 var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor;
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +20,7 @@ for(var i=0; i<squares.length; i++){
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again"
@@ -40,3 +42,26 @@ function pickColor(){
     return colors[random];
 }
 
+function generateRandomColors(num){
+    // make array
+    var arr = [];
+    // add num random colors to array
+    for (var i = 0; i < num; i++){
+    // get random color and push in to array
+        arr.push(randomColor());
+    }
+    // return the array
+    return arr;
+}
+
+function randomColor(){
+    // pick a "red" from 0 to 255
+    var r = Math.floor(Math.random()*256);
+    // pick a "green" from 0 to 255
+    var g = Math.floor(Math.random()*256);
+    // pick a "blue" from 0 to 255
+    var b = Math.floor(Math.random()*256);
+    // insert to string "rgb(r, g, b)"
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+
+}
